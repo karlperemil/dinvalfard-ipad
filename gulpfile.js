4c79d1c8923ce8ba4fb4d83775f12945e0ae7576
Emil Jönsson <karlperemil@gmail.com>
2017-06-26 17:14:02 +0200

    heroku deploy

--- gulpfile.js
+++ gulpfile.js
@@ -20,79 +20,38 @@ var stylus = require('gulp-stylus');
 
 // Compile Sass, Autoprefix and minify
 gulp.task('styles', function() {
-    return gulp.src(themePath + '/css/main.styl')
+    return gulp.src('css/main.styl')
         .pipe(plumber(function(error) {
             gutil.log(gutil.colors.red(error.message));
             this.emit('end');
         }))
         .pipe(sourcemaps.init())
         .pipe(stylus())
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
         .pipe(sourcemaps.write())
         .pipe(concat('app.css'))
-        .pipe(gulp.dest(themePath + '/public/stylesheets/'))
+        .pipe(gulp.dest('public/css'))
         .pipe(rename({suffix: '.min'}))
         .pipe(cssnano())
-        .pipe(gulp.dest(themePath + '/public/stylesheets/'))
+        .pipe(gulp.dest('public/css'))
 });
 
-gulp.task('styles-standalone', function() {
-    return gulp.src(themePath + '/assets/css/standalone/*.styl')
-        .pipe(plumber(function(error) {
-            gutil.log(gutil.colors.red(error.message));
-            this.emit('end');
-        }))
-        .pipe(sourcemaps.init())
-        .pipe(stylus())
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest(themePath + '/compiled/css/standalone'))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(cssnano())
-        .pipe(gulp.dest(themePath + '/compiled/css/standalone'))
-});
-
-gulp.task('styles-vendor', function() {
-    return gulp.src(themePath + '/assets/css/vendor/*.css')
-        .pipe(plumber(function(error) {
-            gutil.log(gutil.colors.red(error.message));
-            this.emit('end');
-        }))
-        .pipe(sourcemaps.init({largeFile: true})) // Start Sourcemaps
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe(concat('vendor.css'))
-        .pipe(gulp.dest(themePath + '/compiled/css/vendor'))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(cssnano())
-        .pipe(sourcemaps.write()) // Creates sourcemaps for minified styles
-        .pipe(gulp.dest(themePath + '/compiled/css/vendor'))
-});
     
 // JSHint, concat, and minify JavaScript 
 gulp.task('js-vendor', function() {
   return gulp.src([	
 	  
            // Grab your custom scripts
-          themePath + '/assets/js/vendor/*.js'
+          'js/vendor/*.js'
   		  
   ])
     .pipe(plumber())
     //.pipe(sourcemaps.init({largeFile: true}))
     .pipe(concat('vendor.js'))
     //.pipe(sourcemaps.write())
-    .pipe(gulp.dest(themePath + '/compiled/js/vendor'))
+    .pipe(gulp.dest('public/js/vendor'))
     .pipe(rename({suffix: '.min'}))
     .pipe(uglify())
-    .pipe(gulp.dest(themePath +'/compiled/js/vendor'))
+    .pipe(gulp.dest('public/js/vendor'))
 });
 
 // JSHint, concat, and minify JavaScript
@@ -100,106 +59,65 @@ gulp.task('js', function() {
   return gulp.src([	
 	  
            // Grab your custom scripts
-          themePath +'/js/*.js'
+          'js/*.js'
   		  
   ])
     .pipe(plumber())
     .pipe(sourcemaps.init())
     .pipe(concat('app.js'))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest(themePath +'/compiled/js'))
+    .pipe(gulp.dest('public/js'))
     .pipe(rename({suffix: '.min'}))
     .pipe(stripDebug())
     .pipe(uglify())
-    .pipe(gulp.dest(themePath +'/compiled/js'))
-});
-
-// JSHint, concat, and minify JavaScript
-gulp.task('js-standalone', function() {
-  return gulp.src([	
-	  
-           // Grab your custom scripts
-          themePath +'/assets/js/standalone/*.js' 
-  		  
-  ])
-    .pipe(plumber())
-    .pipe(sourcemaps.init())
-    .pipe(sourcemaps.write()) // Creates sourcemap for minified JS
-    .pipe(gulp.dest(themePath +'/compiled/js/standalone'))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(stripDebug())
-    .pipe(uglify())
-    .pipe(gulp.dest(themePath +'/compiled/js/standalone'))
-});   
-
-gulp.task('compile-jade', function() {
-  gulp.src(themePath+ '/assets/pages/*.jade')
-    .pipe(jade({
-        locals: {
-          title: 'some local variable'
-        },
-        pretty: true
-     }))
-     .pipe(gulp.dest(themePath+'/compiled/pages'));
+    .pipe(gulp.dest('public/js'))
 });
 
 // Browser-Sync watch files and inject changes
 gulp.task('browsersync', function() {
     // Watch files
     var files = [
-    	themePath + '/assets/css/*.styl',
-        themePath + '/assets/css/standalone/*.styl', 
-    	themePath +'/assets/js/*.js',
-        themePath + '/assets/js/standalone*.js',
-    	themePath + '/**/*.php',
-    	themePath + '/assets/images/**/*.{png,jpg,gif,svg,webp}',
+    	'css/*.styl',
+    	'js/*.js',
+    	'public/images/**/*.{png,jpg,gif,svg,webp}'
     ];
 
     browserSync.init(files, {
 	    // Replace with URL of your local site
-	    proxy: "http://localhost:8888",
+	    proxy: "http://localhost:8888"
     });
     
-    // Watch .scss files
-    gulp.watch(themePath +'/assets/css/*.styl', ['styles']);
-    gulp.watch(themePath +'/assets/css/standalone/*.styl', ['styles-standalone']);
+    // Watch .styl files
+    gulp.watch('css/*.styl', ['styles']);
 
     // Watch site-js files
-    gulp.watch(themePath+'/assets/js/*.js', ['js']);
-    gulp.watch(themePath+'/assets/js/standalone/*.js', ['js-standalone']);
+    gulp.watch('js/*.js', ['js']);
 
-    //compile jade
+    /* compile jade
     gulp.watch(themePath+'/assets/pages/*.jade',['compile-jade']);
+    */
 
 });
 
 // Watch files for changes (without Browser-Sync)
 gulp.task('watch', function() {
 
-  // Watch .scss files
-  gulp.watch(themePath+'/css/*.styl', ['styles']);
-  gulp.watch(themePath+'/css/standalone*.styl', ['styles-standalone']);
+  // Watch .styl files
+  gulp.watch('css/*.styl', ['styles']);
 
   // Watch site-js files
-  gulp.watch(themePath+'/js/*.js', ['js']);
-  gulp.watch(themePath+'/js/standalone*.js', ['js-standalone']);
-  
-  // Watch foundation-js files
-  gulp.watch(themePath+'/vendor/foundation-sites/js/*.js', ['foundation-js']);
-
-  //compile jade
-gulp.watch(themePath+'/assets/pages/*.jade',['compile-jade']);
+  gulp.watch('js/*.js', ['js']);
 
 }); 
 
 // Run styles, site-js and foundation-js
 gulp.task('default', function(done){
-    runSequence('styles','styles-standalone','js','js-standalone','compile-jade',function(){
+    runSequence('styles','js','js-vendor',function(){
         console.log('Done compiling base...');
         gulp.start('browsersync');
     });
 });
 
 gulp.task('build', function(){
-    gulp.start('styles','styles-standalone','styles-vendor','js-vendor','js','js-standalone','foundation-js');
+    gulp.start('styles','js-vendor','js');
 })
--- js/main.js
+++ js/main.js
@@ -1,3 +1,74 @@
+function getUrlVars()
+{
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
+
+var copy = {
+    vardcentral: {
+        introtitle: 'Vill du lägga ner<br>517 av Sveriges<br>vårdcentraler?',
+        introbody: '42% av Sveriges vårdcentraler drivs idag i privat regi. Men nu hotar ett politiskt förslag deras överlevnad. Blir förslaget verklighet innebär det inte bara att det blir längre till din närmsta vårdcentral. Vårdköerna kommer också att bli längre och din valfrihet kommer att minska.',
+        yestitle: 'Tråkigt att du<br>känner dig missnöjd<br>med den privata<br>vården!',
+        yesbody: 'Läs mer om hur din vård och omsorg kan<br>komma att drabbas och visa ditt stöd på<br><a href="http://www.dinvalfard.se">dinvalfard.se</a>',
+        notitle: 'Tack för att du<br>hjälper till att rädda<br>välfärden!',
+        nobody: 'Läs mer om hur din vård och omsorg kan komma att drabbas av det politiska förslaget och visa ditt stöd på<br><a href="http://www.dinvalfard.se">dinvalfard.se</a>'
+    },
+    aldreboenden: {
+        introtitle: 'Vill du lägga ner<br>517 av Sveriges<br>äldreboenden?',
+        introbody: '42% av Sveriges vårdcentraler drivs idag i privat regi. Men nu hotar ett politiskt förslag deras överlevnad. Blir förslaget verklighet innebär det inte bara att det blir längre till din närmsta vårdcentral. Vårdköerna kommer också att bli längre och din valfrihet kommer att minska.',
+        yestitle: 'Tråkigt att du<br>känner dig missnöjd<br>med den privata<br>vården!',
+        yesbody: 'Läs mer om hur din vård och omsorg kan<br>komma att drabbas och visa ditt stöd på<br><a href="http://www.dinvalfard.se">dinvalfard.se</a>',
+        notitle: 'Tack för att du<br>hjälper till att rädda<br>välfärden!',
+        nobody: 'Läs mer om hur din vård och omsorg kan komma att drabbas av det politiska förslaget och visa ditt stöd på<br><a href="http://www.dinvalfard.se">dinvalfard.se</a>'
+    },
+    specialistvard: {
+        introtitle: 'Vill du lägga ner<br>517 av Sveriges<br>specialistvård?',
+        introbody: '42% av Sveriges vårdcentraler drivs idag i privat regi. Men nu hotar ett politiskt förslag deras överlevnad. Blir förslaget verklighet innebär det inte bara att det blir längre till din närmsta vårdcentral. Vårdköerna kommer också att bli längre och din valfrihet kommer att minska.',
+        yestitle: 'Tråkigt att du<br>känner dig missnöjd<br>med den privata<br>vården!',
+        yesbody: 'Läs mer om hur din vård och omsorg kan<br>komma att drabbas och visa ditt stöd på<br><a href="http://www.dinvalfard.se">dinvalfard.se</a>',
+        notitle: 'Tack för att du<br>hjälper till att rädda<br>välfärden!',
+        nobody: 'Läs mer om hur din vård och omsorg kan komma att drabbas av det politiska förslaget och visa ditt stöd på<br><a href="http://www.dinvalfard.se">dinvalfard.se</a>'
+    }
+}
+
 $(document).ready(function(){
     console.log('ready');
+    $('.button-yes').click(function(){
+        $('#question').hide();
+        $('#yes').show();
+        reset();
+    });
+    $('.button-no').click(function(){
+        $('#question').hide();
+        $('#no').show();
+        reset();
+    });
+
+    console.log(getUrlVars());
+    var nameKey = getUrlVars()['namn'];
+    console.log(nameKey);
+    var pageCopy = copy[nameKey];
+    console.log(pageCopy);
+    $('#question h1').html(pageCopy.introtitle);
+    $('#question p').html(pageCopy.introbody);
+    $('#no h2').html(pageCopy.notitle);
+    $('#no p').html(pageCopy.nobody);
+    $('#yes h2').html(pageCopy.yestitle);
+    $('#yes p').html(pageCopy.yesbody);
+
+
 })
+
+function reset(){
+    setTimeout(function(){
+        $('#question').show();
+        $('#no, #yes').hide();
+    },20000);
+}
