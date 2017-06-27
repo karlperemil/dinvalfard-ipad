5b4fd78d3bba5b84159a5ef4e33ea4bdcbaceb0b
Emil Jönsson <karlperemil@gmail.com>
2017-06-27 20:21:33 +0200

    feedback

--- public/js/app.js
+++ public/js/app.js
@@ -14,27 +14,27 @@ function getUrlVars()
 var copy = {
     vardcentral: {
         introtitle: 'Vill du lägga ner<br>517 av Sveriges<br>vårdcentraler?',
-        introbody: '42% av Sveriges vårdcentraler drivs idag i privat regi. Men nu hotar ett politiskt förslag deras överlevnad. Blir förslaget verklighet innebär det inte bara att det blir längre till din närmsta vårdcentral. Vårdköerna kommer också att bli längre och din valfrihet kommer att minska.<br>Vill du det?',
+        introbody: '42 procent av Sveriges vårdcentraler drivs idag i privat regi. Men nu hotar ett politiskt förslag deras överlevnad. Blir förslaget verklighet innebär det inte bara att det blir längre till din närmsta vårdcentral. Vårdköerna kommer också att bli längre och din valfrihet kommer att minska.<br>Vill du det?',
         yestitle: 'Tråkigt att du<br>känner dig missnöjd<br>med den privata<br>vården!',
-        yesbody: 'Läs mer om hur din vård och omsorg kan komma att drabbas av det politiska förslaget på <a href="http://www.dinvalfard.se">dinvalfard.se</a>',
+        yesbody: 'Läs mer om hur din vård och omsorg kan komma att drabbas av det politiska förslaget på <span class="pink">dinvälfärd.se</span>',
         notitle: 'Tack för att du<br>hjälper till att rädda<br>välfärden!',
-        nobody: 'Läs mer om hur det ser ut för din vård och omsorg och visa ditt stöd på<br><a href="http://www.dinvalfard.se">dinvalfard.se</a>'
+        nobody: 'Läs mer om hur det ser ut för din vård och omsorg och visa ditt stöd på<br><span class="pink">dinvälfärd.se</span>'
     },
     aldreboenden: {
         introtitle: 'Vill du lägga ner<br>517 av Sveriges<br>äldreboenden?',
-        introbody: '42% av Sveriges vårdcentraler drivs idag i privat regi. Men nu hotar ett politiskt förslag deras överlevnad. Blir förslaget verklighet innebär det inte bara att det blir längre till din närmsta vårdcentral. Vårdköerna kommer också att bli längre och din valfrihet kommer att minska.',
+        introbody: '42 procent av Sveriges vårdcentraler drivs idag i privat regi. Men nu hotar ett politiskt förslag deras överlevnad. Blir förslaget verklighet innebär det inte bara att det blir längre till din närmsta vårdcentral. Vårdköerna kommer också att bli längre och din valfrihet kommer att minska.',
         yestitle: 'Tråkigt att du<br>känner dig missnöjd<br>med den privata<br>vården!',
         yesbody: 'Läs mer om hur din vård och omsorg kan<br>komma att drabbas och visa ditt stöd på<br><a href="http://www.dinvalfard.se">dinvalfard.se</a>',
         notitle: 'Tack för att du<br>hjälper till att rädda<br>välfärden!',
-        nobody: 'Läs mer om hur din vård och omsorg kan komma att drabbas av det politiska förslaget och visa ditt stöd på<br><a href="http://www.dinvalfard.se">dinvalfard.se</a>'
+        nobody: 'Läs mer om hur din vård och omsorg kan komma att drabbas av det politiska förslaget och visa ditt stöd på<br><span class="pink">dinvälfärd.se</span>'
     },
     specialistvard: {
         introtitle: 'Vill du lägga ner<br>517 av Sveriges<br>specialistvård?',
-        introbody: '42% av Sveriges vårdcentraler drivs idag i privat regi. Men nu hotar ett politiskt förslag deras överlevnad. Blir förslaget verklighet innebär det inte bara att det blir längre till din närmsta vårdcentral. Vårdköerna kommer också att bli längre och din valfrihet kommer att minska.',
+        introbody: '42 procent av Sveriges vårdcentraler drivs idag i privat regi. Men nu hotar ett politiskt förslag deras överlevnad. Blir förslaget verklighet innebär det inte bara att det blir längre till din närmsta vårdcentral. Vårdköerna kommer också att bli längre och din valfrihet kommer att minska.',
         yestitle: 'Tråkigt att du<br>känner dig missnöjd<br>med den privata<br>vården!',
         yesbody: 'Läs mer om hur din vård och omsorg kan<br>komma att drabbas och visa ditt stöd på<br><a href="http://www.dinvalfard.se">dinvalfard.se</a>',
         notitle: 'Tack för att du<br>hjälper till att rädda<br>välfärden!',
-        nobody: 'Läs mer om hur din vård och omsorg kan komma att drabbas av det politiska förslaget och visa ditt stöd på<br><a href="http://www.dinvalfard.se">dinvalfard.se</a>'
+        nobody: 'Läs mer om hur din vård och omsorg kan komma att drabbas av det politiska förslaget och visa ditt stöd på<br><span class="pink">dinvälfärd.se</span>'
     }
 }
 
@@ -55,24 +55,28 @@ $(document).ready(function(){
 
     console.log('ready');
     $('.button-yes').click(function(){
-        $('#question').hide();
-        $('#yes').show();
-        reset();
-        ga('send', {
-            hitType: 'event',
-            eventCategory: trackingName,
-            eventAction: 'Ja'
-        });
+        setTimeout(function(){
+            $('#question').hide();
+            $('#yes').show();
+            reset();
+            ga('send', {
+                hitType: 'event',
+                eventCategory: trackingName,
+                eventAction: 'Ja'
+            });
+        },500);
     });
     $('.button-no').click(function(){
-        $('#question').hide();
-        $('#no').show();
-        reset();
-        ga('send', {
-            hitType: 'event',
-            eventCategory: trackingName,
-            eventAction: 'Nej'
-        });
+        setTimeout(function(){
+            $('#question').hide();
+            $('#no').show();
+            reset();
+            ga('send', {
+                hitType: 'event',
+                eventCategory: trackingName,
+                eventAction: 'Nej'
+            });
+        },500);
     });
 })
 
@@ -80,10 +84,11 @@ function reset(){
     setTimeout(function(){
         $('#question').show();
         $('#no, #yes').hide();
+        $('.button-yes, .button-no').blur();
         ga('send', {
             hitType: 'event',
             eventCategory: trackingName,
             eventAction: 'reset page'
         });
-    },20000);
+    },60000);
 }
